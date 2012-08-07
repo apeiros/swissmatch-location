ecd8f01920324a7bdefab812b96a8584b767cd59
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2012-08-07 13:56:01 +0200

    Added the new hub-file swissmatch/location (does not autoload the data, though).

---
+++ lib/swissmatch/location.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+
+
+require 'swissmatch/canton'
+require 'swissmatch/cantons'
+require 'swissmatch/communities'
+require 'swissmatch/community'
+require 'swissmatch/zipcode'
+require 'swissmatch/zipcodes'
