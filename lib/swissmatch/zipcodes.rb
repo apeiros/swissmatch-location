586fac4c97a6836a626d52cdbdf912fd08556833
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2012-08-08 20:54:30 +0200

    Removed select! and reject! again. The SwissMatch collection classes should be immutable.

--- lib/swissmatch/zipcodes.rb
+++ lib/swissmatch/zipcodes.rb
@@ -241,26 +241,6 @@ module SwissMatch
       ZipCodes.new(@zip_codes.reject(*args, &block))
     end
 
-    # @return [self]
-    #   Removes all zip codes from the collection for which the block did not return a true (or 
-    #   a trueish value)
-    def select!(*args, &block)
-      @zip_codes.select!(*args, &block)
-      reset!
-
-      self
-    end
-
-    # @return [self]
-    #   Removes all zip codes from the collection for which the block did not return a false (or 
-    #   a falseish value)
-    def reject!(*args, &block)
-      @zip_codes.reject!(*args, &block)
-      reset!
-
-      self
-    end
-
     # @return [Integer] The number of SwissMatch::ZipCode objects in this collection.
     def size
       @zip_codes.size
